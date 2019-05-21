0f1ef440a182b6a1831dd238c9e23677248ea307
Krithikasri <krithisri412@gmail.com>
2019-05-21 12:45:29 +0530

    Create name.py

---
+++ name.py
@@ -0,0 +1,7 @@
+def print_full_name(a, b):
+    x=first_name +" "+ last_name+"!"
+    print("Hello",x,"You just delved into python.")
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, l
