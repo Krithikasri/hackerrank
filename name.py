189cc4556d274740836608aebdb347516e64bdf2
Krithikasri <krithisri412@gmail.com>
2019-05-27 22:53:40 +0530

    Update name.py

--- name.py
+++ name.py
@@ -4,4 +4,4 @@ def print_full_name(a, b):
 if __name__ == '__main__':
     first_name = input()
     last_name = input()
-    print_full_name(first_name, l
+    print_full_name(first_name, last_name)
